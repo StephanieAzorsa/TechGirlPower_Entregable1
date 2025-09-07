ff3b85b8be89cf1d518fe724dd9b1ef85733042f
Stephanie <stephanie.azorsa@unmsm.edu.pe>
2025-09-06 21:03:15 -0500

    [FEAT]: Add WebClient to connect account-service

---
+++ services/transaction-service/src/main/java/com/nttdata/transactionservice/client/Account.java
@@ -0,0 +1,12 @@
+package com.nttdata.transactionservice.client;
+
+import java.math.BigDecimal;
+
+public class Account {
+
+    private String id;
+    private String accountNumber;
+    private BigDecimal balance;
+    private Long customerId;
+
+}
