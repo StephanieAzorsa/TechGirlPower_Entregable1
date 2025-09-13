ee3ac2e4352d6dff0e81564d251bbc6f164062ad
Stephanie <stephanie.azorsa@unmsm.edu.pe>
2025-09-13 00:45:05 -0500

    [FEAT]: Add Account constants to MAX_OVERDRAFT and MIN_SAVINGS_BALANCE

---
+++ services/account-service/src/main/java/com/nttdata/accountservice/util/AccountConstants.java
@@ -0,0 +1,8 @@
+package com.nttdata.accountservice.util;
+
+import java.math.BigDecimal;
+
+public class AccountConstants {
+    public static final BigDecimal MAX_OVERDRAFT = new BigDecimal("-500");
+    public static final BigDecimal MIN_SAVINGS_BALANCE = BigDecimal.ZERO;
+}
