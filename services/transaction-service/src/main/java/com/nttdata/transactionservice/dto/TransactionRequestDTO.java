8b651f860d0d9255b217aa79d0901ecb552c955a
Stephanie <stephanie.azorsa@unmsm.edu.pe>
2025-09-21 03:38:05 -0500

    [FEATURE]: Add @NoArgsConstructor and @AllArgsConstructor

--- services/transaction-service/src/main/java/com/nttdata/transactionservice/dto/TransactionRequestDTO.java
+++ services/transaction-service/src/main/java/com/nttdata/transactionservice/dto/TransactionRequestDTO.java
@@ -3,13 +3,17 @@ package com.nttdata.transactionservice.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class TransactionRequestDTO {
     @NotBlank(message = "Account ID es requerido")
     private String accountId;
--- services/transaction-service/src/main/java/com/nttdata/transactionservice/dto/TransferRequestDTO.java
+++ services/transaction-service/src/main/java/com/nttdata/transactionservice/dto/TransferRequestDTO.java
@@ -3,13 +3,17 @@ package com.nttdata.transactionservice.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class TransferRequestDTO {
     @NotBlank(message = "Cuenta origen es requerida")
     private String sourceAccountId;
